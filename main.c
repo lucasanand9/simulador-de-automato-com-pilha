3c1a1d43c4f2b25920930354904cca9a87f5b491
lucasanand9 <lucasanand9@gmail.com>
2024-10-15 14:57:45 -0300

    fazendo a leitura dos estados

--- main.c
+++ main.c
@@ -4,9 +4,17 @@
 
 int main(){
     Estados *estados;
+
     int index = 0;
-    printf("Digite quais sao os estadosnf(digite -1 apara parar):\n");
+    printf("Digite quais sao os estados:\n");
+    int quantidadeEstados = 0;
     while (index != -1){
-        
+        quantidadeEstados++;
+        printf("Qual o nome do novo estado\n");
+        estados = malloc(sizeof(Estados)*quantidadeEstados);
+        scanf(" %s", estados[quantidadeEstados-1]);
+
+        printf("Digite 0 para adicionar um novo estado e -1 para sair:/n");
+        scanf("%i", &index);
     }
 }
